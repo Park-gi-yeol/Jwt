baac0b9f718622f77179bf90101fd26a2868b768
Park-Gi-Yeol <v261131@naver.com>
2026-04-03 16:27:13 +0900

    first commit

---
+++ src/main/java/com/jwt/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.jwt.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@GetMapping("/hello")
+	public ResponseEntity<String> adminP() {
+		return ResponseEntity.ok("adminc");
+	}
+}
---
+++ src/main/java/com/jwt/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.jwt.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+	
+	@GetMapping("/")
+	public ResponseEntity<String> mainP() {
+		return ResponseEntity.ok("mainc");	
+	}
+	
+}
